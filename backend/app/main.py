85658c08c9eefe0585fdc0290ffea460925ccb61
Anastasiia <anastasiia.humeniuk.shi.2024@lpnu.ua>
2026-03-20 22:11:57 +0200

    Connect to backend

--- backend/app/main.py
+++ backend/app/main.py
@@ -28,17 +28,10 @@ async def lifespan(app: FastAPI):
 
 app = FastAPI(lifespan=lifespan)
 
-origins = [
-    "http://localhost:5173",
-    "http://127.0.0.1:5173",
-    "http://localhost:5500",
-    "http://127.0.0.1:5500",
-]
-
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
+    allow_origins=["*"],
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
